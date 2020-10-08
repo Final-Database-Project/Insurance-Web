604b4b901ae10664beb034bbc430f7fbcfe6d7a4
Victor Jose <jose@pop-os.localdomain>
2020-10-08 16:39:39 -0400

    front

---
+++ vuetify-front/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+  <v-app>
+    <v-app-bar app dark>
+      <div class="d-flex align-center mr-2">
+        bezKoder
+      </div>
+
+      <v-btn to="/tutorials" text>
+        Tutorials
+      </v-btn>
+
+      <v-btn to="/add" text>
+        Add
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
