2a3c4434964efb3a0d6862680ab0677f83e20f61
kelvinlee0225 <leekelvin0225@gmail.com>
2020-10-11 00:26:37 -0400

    Added Delete Function

--- controladores/home/home.controlador.js
+++ controladores/home/home.controlador.js
@@ -8,7 +8,7 @@ exports.findByEmail = (req, res) => {
 	}
 
 	const home = new Home({
-		Correo: req.query.Correo,
+		Correo: req.query.Correo
 	});
 
 	Home.findByEmail(home, (err, data) => {
@@ -23,6 +23,35 @@ exports.findByEmail = (req, res) => {
 						err.message ||
 						"Ha ocurrido un error al intentar buscar el perfil del usuario con el correo " +
 							req.query.Correo,
+				});
+			}
+		else res.send(data);
+	});
+};
+
+exports.DeleteSeguro = (req, res) => {
+	if (!req.params) {
+		res.status(400).send({
+			message: "Tiene que elegir por lo menos un objeto!",
+		});
+	}
+
+	const home = new Home({
+		idSeguro: req.query.idSeguro
+	});
+
+	Home.DeleteSeguro(home, (err, data) => {
+		if (err)
+			if ((err.kind = "no encontrado")) {
+				res.status(404).send({
+					message: `El usuario con el correo ${req.query.idSeguro} no puedo ser encontrado`,
+				});
+			} else {
+				res.status(500).send({
+					message:
+						err.message ||
+						"Ha ocurrido un error al intentar buscar el Seguro" +
+							req.query.idSeguro,
 				});
 			}
 		else res.send(data);
--- modelos/home/modelo.home.js
+++ modelos/home/modelo.home.js
@@ -2,6 +2,7 @@ const sql = require("../../src/index");
 
 const Home = function Home(home) {
 	this.Correo = home.Correo;
+	this.idSeguro = home.idSeguro;
 };
 
 Home.findByEmail = (nuevohome, result) => {
@@ -43,6 +44,111 @@ Home.findByEmail = (nuevohome, result) => {
 			} else if (idTipoUsuario == 2) {
 				request.execute(
 					"SP_Home_Personal",
+					(err, res) => {
+						if (err) {
+							console.log("error: ", err);
+							result(err, null);
+							return;
+						}
+						if (res.rowsAffected == 0) {
+							result({ kind: "no encontrado" }, null);
+							return;
+						}
+						result(null, res.recordset[0]);
+					}
+				);
+			}
+		}
+	);
+};
+
+Home.DeleteSeguro = (nuevohome, result) => {
+	const request = sql.request();
+	var idSeguro = "";
+	request.input("idSeguro", nuevohome.idSeguro);
+	request.query(
+		"Select idSeguro from Seguro where idSeguro = @idSeguro",
+		(err, res) => {
+			if (err) {
+				console.log("error: ", err);
+				result(err, null);
+				return;
+			}
+			if (res.rowsAffected == 0) {
+				result({ kind: "no encontrado" }, null);
+				return;
+			}
+
+			idTipoSeguro = res.recordset[0].idTipoSeguro;
+
+			if (idTipoSeguro == 1) {
+				request.execute(
+					"Delete_Seguro_plus_VidaColectiva",
+					(err, res) => {
+						if (err) {
+							console.log("error: ", err);
+							result(err, null);
+							return;
+						}
+						if (res.rowsAffected == 0) {
+							result({ kind: "no encontrado" }, null);
+							return;
+						}
+
+						result(null, null);
+					}
+				);
+			} else if (idSeguro == 2) {
+				request.execute(
+					"Delete_Seguro_plus_Incendio",
+					(err, res) => {
+						if (err) {
+							console.log("error: ", err);
+							result(err, null);
+							return;
+						}
+						if (res.rowsAffected == 0) {
+							result({ kind: "no encontrado" }, null);
+							return;
+						}
+						result(null, res.recordset[0]);
+					}
+				);
+			} else if (idSeguro == 3) {
+				request.execute(
+					"Delete_Seguro_plus_MedioTransporte",
+					(err, res) => {
+						if (err) {
+							console.log("error: ", err);
+							result(err, null);
+							return;
+						}
+						if (res.rowsAffected == 0) {
+							result({ kind: "no encontrado" }, null);
+							return;
+						}
+						result(null, res.recordset[0]);
+					}
+				);
+			} else if (idSeguro == 4) {
+				request.execute(
+					"Delete_Seguro_plus_Vida",
+					(err, res) => {
+						if (err) {
+							console.log("error: ", err);
+							result(err, null);
+							return;
+						}
+						if (res.rowsAffected == 0) {
+							result({ kind: "no encontrado" }, null);
+							return;
+						}
+						result(null, res.recordset[0]);
+					}
+				);
+			} else if (idSeguro == 5) {
+				request.execute(
+					"Delete_Seguro_plus_Salud",
 					(err, res) => {
 						if (err) {
 							console.log("error: ", err);
